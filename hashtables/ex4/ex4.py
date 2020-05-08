217a0a523406762f823ef663bb324f44ea04eff7
Kumar Veeravel <mvkumar14@gmail.com>
2020-05-08 08:23:43 -0700

    finished ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,19 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
+    # for every item
+    # if dict[-item] exists
+        # add to result
+    # else (except KeyError)
+    # add dict[item]
+    result = []
+    storage = {}
+    for item in a:
+        try:
+            if storage[-item] == 1:
+                result.append(abs(item))
+        except KeyError:
+            storage[item] = 1
 
     return result
 
