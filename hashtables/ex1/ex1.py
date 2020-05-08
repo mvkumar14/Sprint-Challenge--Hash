0b119560e8c8c01b2856cad5ee82a2c68312f4a1
Kumar Veeravel <mvkumar14@gmail.com>
2020-05-08 08:55:16 -0700

    finished ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,27 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    # for every item
+    # if storage[limit - item] exists:
+        # compose and return result
+    # else
+        # add the weight:index to the 
+        # storage
+    
+    if length < 1:
+        return None
 
-    return None
+    storage = {}
+    output = []
+    possible = False
+    for index,i in enumerate(weights):
+        try:
+            test = storage[limit-i] # should return index
+            return (index, test)
+        except KeyError:
+            storage[i] = index
+    
+    if possible:
+        return (storage[max(output)],storage[min(output)])
+    else: 
+        return None
